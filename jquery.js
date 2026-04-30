9bce8fdf7a2561261cc955139455bdacf9aa5a4a
David04112004 <david.azoulay@edu.ece.fr>
2026-04-30 10:34:50 +0200

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -4,6 +4,20 @@ $(document).ready(function () {
         $("#menu").slideToggle(300);
     });
 
+    $("#listeProjets").hide();
+
+    $("#btnProjets").click(function () {
+        $("#listeProjets").slideToggle(300);
+    });
+
+    $(".contenu-projet").hide();
+
+    $(".titre-projet").click(function () {
+        let contenu = $(this).next();
+        $(".contenu-projet").not(contenu).slideUp(300);
+        contenu.slideToggle(300);
+    });
+
     $("#contenuAccordeon").hide();
 
     $("#btnAccordeon").click(function () {
@@ -11,3 +25,30 @@ $(document).ready(function () {
     });
 
 });
+
+// CAROUSEL
+
+let index = 0;
+let slides = $(".slide");
+
+function showSlide(i) {
+    slides.removeClass("active");
+    slides.eq(i).addClass("active");
+}
+
+showSlide(index);
+
+$("#next").click(function () {
+    index++;
+    if (index >= slides.length) index = 0;
+    showSlide(index);
+});
+
+$("#prev").click(function () {
+    index--;
+    if (index < 0) index = slides.length - 1;
+    showSlide(index);
+});
+setInterval(function(){
+    $("#next").click();
+}, 4000);
